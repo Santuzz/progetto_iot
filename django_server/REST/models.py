599246ca905d1a5beb34704dd2c3dbd6bc7aa124
Davide <davi9898@gmail.com>
2024-02-20 07:56:26 +0100

    models added

--- django_server/REST/models.py
+++ django_server/REST/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
+# from geopy.distance import geodesic
 
-# Create your models here.
+
+class Crossroad(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    creation_date = models.DateTimeField(
+        auto_now_add=True, blank=True, null=True)
+    traffic_level = models.FloatField(default=0)
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Webcam(models.Model):
+    id = models.AutoField(primary_key=True)
+    cars_count = models.IntegerField(default=0)
+    active = models.BooleanField(default=False)
+    crossroad = models.ForeignKey(
+        Crossroad, on_delete=models.CASCADE, null=True)
+    # per avere una sola webcam ad incrocio sostituire crossroad con il seguente field
+    # crossroad = models.OneToOneField(
+    #    Crossroad, on_delete=models.CASCADE, primary_key=True)
+
+
+class Trafficlight(models.Model):
+    DIRECTION_CHOICES = [
+        ('LS', 'Left straight'),
+        ('L', 'Left'),
+        ('RS', 'Right straight'),
+        ('R', 'Right'),
+        ('A', 'Any'),
+        ('S', 'Straight')
+    ]
+    id = models.AutoField(primary_key=True)
+    direction = models.CharField(choices=DIRECTION_CHOICES)
+    green_probability = models.FloatField(default=0.5)
+    crossroad = models.ForeignKey(
+        Crossroad, on_delete=models.CASCADE, null=True)
+
+# TODO Sensore del particolato per la rilevazione della qualità dell'aria
--- django_server/REST/views.py
+++ django_server/REST/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET','POST','DELETE'])
+class WebCamAPI(APIView):
+
+
+    
