37de366c88cd5c7150c2ad1296ef7f0c3a50390b
Davide <davi9898@gmail.com>
2024-03-01 11:48:52 +0100

    Aggiunto Street model

--- django_server/REST/models.py
+++ django_server/REST/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
+from django.contrib.postgres.fields import ArrayField
 # from geopy.distance import geodesic
-
-# TODO Aggiungere un attributo che indichi quante strade sono presenti nell'incrocio
-# streets=models.IntegerField(default=4)
 
 
 class Crossroad(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
     creation_date = models.DateTimeField(
         auto_now_add=True, blank=True, null=True)
     traffic_level = models.FloatField(default=0)
@@ -19,17 +17,26 @@ class Crossroad(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+
+class Street(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    length = models.IntegerField(default=100)
+    alert = models.BooleanField(default=False)
+    Crossroad = models.ManyToManyField(Crossroad, blank=True)
+
+
 # TODO modificare campo cars_count in modo che contenga un array di N int (numero di macchine per ogni strada)
 # cars_count=ArrayField(models.IntegerField(null=True, blank=True),default=[0, 0, 0, 0], null=True, blank=True)
     #                                                                      ^__________^ => N
-
-
 class Webcam(models.Model):
     id = models.AutoField(primary_key=True)
-    cars_count = models.IntegerField(default=0)
+    cars_count = models.CharField(max_length=100, default="0,0,0,0")
     active = models.BooleanField(default=False)
     crossroad = models.ForeignKey(
         Crossroad, on_delete=models.CASCADE, null=True, blank=True)
+
+    def get_list_count(self):
+        return self.cars_count.split(',')
 
 
 class Trafficlight(models.Model):
@@ -46,5 +53,7 @@ class Trafficlight(models.Model):
     green_probability = models.FloatField(default=0.5)
     crossroad = models.ForeignKey(
         Crossroad, on_delete=models.CASCADE, null=True, blank=True)
+    street = models.ForeignKey(
+        Street, on_delete=models.CASCADE, null=True, blank=True)
 
 # TODO Sensore del particolato per la rilevazione della qualità dell'aria
