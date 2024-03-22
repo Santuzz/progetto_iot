f35a9580f5c82d9302a0b1966d3e5e2b2ceeb6dd
Davide <davi9898@gmail.com>
2024-03-22 10:27:14 +0100

    inutilizzato

---
+++ bridge/crossroad_request.py
@@ -0,0 +1,19 @@
+from REST_comunication import RestAPI
+import requests
+import sys
+
+ip_address = "localhost"
+
+
+def main():
+    master = RestAPI(user={'email': 'admin@admin.com', 'password': 'admin',
+                     'username': 'admin'}, base_url="http://"+ip_address+":8000/api/")
+    data = {
+        "name": "Giorgio",
+
+    }
+    master.create_instance("crossroad", data)
+
+
+if __name__ == "__main__":
+    main()
